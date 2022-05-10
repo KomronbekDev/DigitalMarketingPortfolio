d5d8299a5f45e88a9217b67775e8165c0ea9902f
KomronbekDev <masterclass815@gmail.com>
2022-05-11 02:31:44 +0500

    commit

---
+++ script.js
@@ -0,0 +1,56 @@
+window.sr = new ScrollReveal()
+sr.reveal('.mainBackground', {
+    delay: 100,
+    distance: '200px',
+    origin: 'right'
+})
+sr.reveal('.contentOneH1', {
+    delay: 100,
+    distance: '200px',
+    origin: 'left'
+})
+sr.reveal('.contentOneP', {
+    delay: 200,
+    distance: '200px',
+    origin: 'left'
+})
+sr.reveal('.profileBox img', {
+    delay: 300,
+    distance: '200px',
+    origin: 'left'
+})
+sr.reveal('.profileOne h3', {
+    delay: 400,
+    distance: '200px',
+    origin: 'left'
+})
+sr.reveal('.profileOne ul', {
+    delay: 500,
+    distance: '200px',
+    origin: 'left'
+})
+sr.reveal('.contentOneButton', {
+    delay: 600,
+    distance: '200px',
+    origin: 'left'
+})
+sr.reveal('.mainContentTwo h3', {
+    delay: 300,
+    distance: '200px',
+})
+sr.reveal('.mainContentTwo p', {
+    delay: 300,
+    distance: '200px',
+})
+sr.reveal('.mainContentTwo h1', {
+    delay: 300,
+    distance: '200px',
+})
+sr.reveal('.mainContentTwo img', {
+    delay: 300,
+    distance: '200px',
+})
+sr.reveal('.mainContentTwo button', {
+    delay: 300,
+    distance: '200px',
+})
